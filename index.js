3d033880d6764d14d44448872c75947efdebd08c
string theorist <119330240+kurbus@users.noreply.github.com>
2022-12-12 20:26:28 +0000

    fix clutter

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ server.on('message', function(msg, rinfo) {
   arr[arr.findlLast]=msg;
   //send back first msg in list to all listening on that port
   server.send(arr[0], 0, arr[0].length, port, hostname, function(err, bytes) {
-    console.log("sent back: " + msg);
+    console.log("corresponded!");
   });
+//bump up the next item in the queue, keeps the line moving
  arr.splice(0,1);
 });
