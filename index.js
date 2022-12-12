ef9036c7c7ba6e0551cd93dfe03180a565e2193f
string theorist <119330240+kurbus@users.noreply.github.com>
2022-12-12 20:28:11 +0000

    ditto

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ server.bind(port, hostname);
 server.on('message', function(msg, rinfo) {
   //append to array's first blank index
   arr[arr.findlLast]=msg;
+  console.log("we got a message")
   //send back first msg in list to all listening on that port
   server.send(arr[0], 0, arr[0].length, port, hostname, function(err, bytes) {
-    console.log("corresponded!");
+    console.log("corresponded! \n");
   });
 //bump up the next item in the queue, keeps the line moving
  arr.splice(0,1);
